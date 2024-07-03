2577472785333ef6f503699e4b62b59037d4fa05
Devarsh Chandiwade <119281406+IMxSPYDER@users.noreply.github.com>
2024-07-03 15:55:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import pandas_datareader as data
 from yahoo_fin import stock_info as si
 from keras.models import load_model
